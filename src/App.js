497b6b9f1eb54db01028499fbfd4fd0a02dd3110
Kounakis Ioannis <ioannis.kounakis@asurion.com>
2018-01-01 18:21:37 +0900

    List Dataflows

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {dataflows: ''}
+  }
+
+  componentDidMount() {
+    axios.get('http://dataservices.imf.org/REST/SDMX_JSON.svc/Dataflow')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          dataflows: response.data.Structure.Dataflows.Dataflow
+                      .map(dataflow =>
+                        <li key={dataflow['@id']}>
+                          {dataflow.Name['#text']}
+                        </li>
+                      )
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <h1 className="App-title">Welcome to IMF Data Explorer</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="App-intro">
+          {this.state.dataflows}
+        </ul>
       </div>
     );
   }
